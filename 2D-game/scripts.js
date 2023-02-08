9b614b6b8704e6735100ef686f82a516dddd1fa7
MyPayNepal <link2bj@gmail.com>
2023-02-08 13:04:37 +0545

    bullets added

--- 2D-game/scripts.js
+++ 2D-game/scripts.js
@@ -1,31 +1,54 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = this.innerWidth;
-    canvas.height = this.innerHeight;
+    canvas.width = 1200;
+    canvas.height = 580;
 
     
     //Keeps track of specified user inputs, Eg. arrow keys.
     class InputHandler{
         constructor(game){
             this.game = game;
-            window.addEventListener('keydown', function(e){  
-                if(e.key ==='ArrowUp'){
+            window.addEventListener('keydown', (e)=>{  
+                if(( 
+                    (e.key ==='ArrowUp') || 
+                    (e.key ==='ArrowDown')
+                    )&& (this.game.keys.indexOf(e.key) === -1) ){
                     this.game.keys.push(e.key);
-                }                                
-                console.log(this.game.keys);
+                }else if(e.key ===' '){
+                    this.game.player.shootTop();
+                }                               
             });
 
-            window.addEventListener('keyup', function(e){
+            window.addEventListener('keyup', (e)=>{
                 if(this.game.keys.indexOf(e.key) > -1){
                     this.game.keys.splice(this.game.keys.indexOf(e.key),1);
                 }
-                console.log(this.game.keys);
             });
         }
     }
     //Handles player lasers
     class Projectile{
+
+        constructor(game, x, y){
+            this.game = game;
+            this.width = 20;
+            this.height = 10;
+            this.x = x;
+            this.y = y;
+            this.speed = 3
+            this.markedForDeletion = false;
+        }
+
+        update(){
+            this.x += this.speed;
+            if(this.x > this.game.width * 0.8) this.markedForDeletion = true;
+        }
+
+        draw(context){
+            context.fillStyle = 'DarkCyan';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
     //handle falling particles (Eg. corks, screws, bolts etc. that comes from damaged enemies.)
@@ -34,21 +57,43 @@ window.addEventListener('load', function(){
     }
     //Handle functions of main characters
     class Player{
+
         constructor(game){
             this.game = game;
-            this.width = 120;
+            this.width = 80;
             this.height = 190;
             this.x = 20;
             this.y = 100;
-            this.speedY = 2;
+            this.speedY = 0
+            this.maxSpeed = 4;
+            this.projectiles = [];
         }
 
         update(){
+            if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if(this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
             this.y += this.speedY;
+            this.projectiles.forEach( projectile =>{
+                projectile.update();
+            });
+            this.projectiles = this.projectiles.filter(projectile=> !projectile.markedForDeletion);
         }
         draw(context){
-            context.fillStyle = 'gold';
+            context.fillStyle = 'LightCoral';
             context.fillRect(this.x, this.y, this.width, this.height);
+            this.projectiles.forEach(projectile=>{
+                projectile.draw(context);
+            })
+        }
+
+        shootTop(){
+            if(this.game.ammo > 0){
+                this.projectiles.push(new Projectile(this.game, this.x + 80, this.y));
+                this.game.ammo--;
+            }
+            
+          
         }
 
     }
@@ -76,10 +121,21 @@ window.addEventListener('load', function(){
             this.height = height;
             this.player = new Player(this);
             this.input = new InputHandler(this);
+            this.keys = [];
+            this.ammo = 20;
+            this.maxAmmo = 50;
+            this.ammoTimer = 0;
+            this.ammoInterval = 500;
         }
 
-        update(){
+        update(deltaTime){
             this.player.update();
+            if(this.ammoTimer > this.ammoInterval){
+                if(this.ammo < this.maxAmmo) this.ammo++;
+                this.ammoTimer = 0;
+            }else{
+                this.ammoTimer += deltaTime;
+            }
         }
         draw(context){
             this.player.draw(context);
@@ -88,16 +144,18 @@ window.addEventListener('load', function(){
 
 
     const game = new Game(canvas.width, canvas.height);
-
+    let lastTime = 0;
     //animation loop
-    function animate(){
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0,0,canvas.width, canvas.height);
-        game.update();
+        game.update(deltaTime);
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
 
-    animate();
+    animate(0);
 
 });
 
